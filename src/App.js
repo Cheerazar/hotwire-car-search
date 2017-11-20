d92282ded3c5d1034d4a31d64aea2396e4bb6c3b
Gunnari Auvinen <gauvinen@gmail.com>
2017-11-20 10:51:12 -0500

    Add basic styling for App component.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
+import { Div } from 'glamorous';
+
 import './App.css';
 
 class App extends Component {
   render () {
     return (
-      <div className="App">
+      <Div className="App-container">
         <header className="App-header">
           <h1 className="App-title">Hotwire Rental Car Search</h1>
         </header>
-      </div>
+      </Div>
     );
   }
 }
